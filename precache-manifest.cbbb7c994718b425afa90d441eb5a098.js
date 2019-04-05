ff91f3314ec8d5bb618a1b539957c00ca5e039ca
Alexandr Kulakov <BenuelBertran@gmail.com>
2019-04-05 20:54:45 +0300

    Add current Build to gh-pages

---
+++ precache-manifest.cbbb7c994718b425afa90d441eb5a098.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2176f44fea57fd2a958b5c0b36ecc465",
+    "url": "/nyam/static/media/Exo2-Thin-100.2176f44f.woff"
+  },
+  {
+    "revision": "0d1fe8cdd98ef164c788",
+    "url": "/nyam/static/css/main.af1f9c46.chunk.css"
+  },
+  {
+    "revision": "fe19e70b07d83307bee3",
+    "url": "/nyam/static/js/runtime~main.8bee7290.js"
+  },
+  {
+    "revision": "10094c9d2eaf728e1ed2",
+    "url": "/nyam/static/js/2.ec51f06e.chunk.js"
+  },
+  {
+    "revision": "6f0a27bfa8bdc6fd861a343db5852bcb",
+    "url": "/nyam/static/media/TrebuchetMS-Bold-700.6f0a27bf.woff2"
+  },
+  {
+    "revision": "09554dbf9f05318184de09f8d838e36b",
+    "url": "/nyam/static/media/Exo2-Thin-100.09554dbf.woff2"
+  },
+  {
+    "revision": "35aa1d26e6139f06c483fb67a1edde86",
+    "url": "/nyam/static/media/TrebuchetMS-Regular-400.35aa1d26.woff2"
+  },
+  {
+    "revision": "1d67731ff8acac50bb90f30f69fb90fa",
+    "url": "/nyam/static/media/page-bg@1x.1d67731f.png"
+  },
+  {
+    "revision": "0d1fe8cdd98ef164c788",
+    "url": "/nyam/static/js/main.e38c89d2.chunk.js"
+  },
+  {
+    "revision": "9b45a5e6fd7e43502d74c1cf2bb77ded",
+    "url": "/nyam/static/media/TrebuchetMS-Bold-700.9b45a5e6.woff"
+  },
+  {
+    "revision": "34723a2fbef5882977e8392d020956af",
+    "url": "/nyam/static/media/TrebuchetMS-Regular-400.34723a2f.woff"
+  },
+  {
+    "revision": "68894ae33dcb13ebb6a0963687b5f805",
+    "url": "/nyam/static/media/card-bg@1x.68894ae3.png"
+  },
+  {
+    "revision": "30d9c363bb057af8a0bfd4953adba436",
+    "url": "/nyam/static/media/card-bg@1x.30d9c363.webp"
+  },
+  {
+    "revision": "1bc3d597738eac39dcc44c6ecbe50c6a",
+    "url": "/nyam/static/media/card-bg@2x.1bc3d597.png"
+  },
+  {
+    "revision": "a97d683ed15e8b575504da34df80d8aa",
+    "url": "/nyam/static/media/card-bg@2x.a97d683e.webp"
+  },
+  {
+    "revision": "e9d4e5169eb901a0ccb1620c35809cdb",
+    "url": "/nyam/index.html"
+  }
+];
